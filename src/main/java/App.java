63269edf6cfe42b43b8ece63dd9e2b31b97d8ec9
G-HanVV <62301594+G-HanVV@users.noreply.github.com>
2022-08-30 21:12:28 +0300

    first try

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat cat = applicationContext.getBean(Cat.class);
+//        System.out.println(cat);
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        System.out.println(cat == cat1);
     }
 }
